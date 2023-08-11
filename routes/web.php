d981f6fca3d6fc7d73692451aac5c5f5fbfad29d
Mojahidul Islam <bdemon245@gmail.com>
2023-08-11 23:50:22 +0600

    redirect to admin route

--- routes/web.php
+++ routes/web.php
@@ -16,44 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $parties = [
-        'হেলেন ট্রেড' => [
-            'name' => 'BHL',
-            'address' => 'জগদীশমোড়, হবীগঞ্জ'
-        ],
-        'প্রাণ RFL' => [
-            'name' => null,
-            'address' => 'অলিপুর, হবীগঞ্জ'
-        ],
-        'প্রাণ বেভারেজ লিঃ' => [
-            'name' => null,
-            'address' => 'গোড়াশাল'
-        ],
-        'তৌহিদী লাইন্স' => [
-            'name' => '',
-            'address' => 'KEPZ'
-        ],
-        'জয়নাল কার্গো' => [
-            'name' => '',
-            'address' => ''
-        ],
-        'করিম ক্যারিয়ার' => [
-            'name' => '',
-            'address' => ''
-        ],
-        'চারু সিরামিক্স লিঃ' => [
-            'name' => null,
-            'address' => 'হবীগঞ্জ'
-        ],
-        'গ্রেট ওয়াল' => [
-            'name' => 'গ্রেট ওয়াল সিরামিক্স লিঃ',
-            'address' => 'মাওনা'
-        ],
-    ];
-    foreach ($parties as $key => $party) {
-        dd($key);
-    }
-    return view('welcome');
+    return redirect(route('filament.pages.dashboard'));
 });
 Route::get('gpt/{text}', function ($text) {
     $gpt = new TranslateGPT();
