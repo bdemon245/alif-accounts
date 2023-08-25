d57d3b7df75c32dddbc6f3a89e320e120a902ace
Mojahidul Islam <bdemon245@gmail.com>
2023-08-25 18:23:15 +0600

    changed domain for user to my domain

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@admin.com');
+        return str_ends_with($this->email, '@wisedevx.xyz');
     }
 }
